0fee21534701c85dc079c7a8472c07bf2edf7178
PabloMaj <majpaw1996@gmail.com>
2026-05-24 23:43:14 +0200

    standarize path to model

--- src/agents/dl_model_trainer_agent.py
+++ src/agents/dl_model_trainer_agent.py
@@ -13,11 +13,17 @@ class DLModelTrainerAgent:
 
         dataset_path = state.get("dataset_path")
 
+        dataset_path_obj = Path(dataset_path)
+        dataset_name = dataset_path_obj.name
+        dataset_group = dataset_path_obj.parent.name
+
+        artifacts_path = Path("yolo_train_artifacts") / dataset_group / f"{dataset_name}_roi640"
+
         pipeline = YOLOPipeline(
             raw_dataset=Path(dataset_path),
             processed_dataset=Path(dataset_path + "_roi640"),
-            yolo_train_artifacts_save_path=Path(r"C:\projects\agent_cv\yolo_train_artifacts\crop_line_uav\maize_3_nerac_2016_1_roi640"),
-            class_names=["crop"]
+            yolo_train_artifacts_save_path=artifacts_path,
+            class_names=["object"]
         )
         pipeline.run()
 
